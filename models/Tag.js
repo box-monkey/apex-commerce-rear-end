ea3058426fd435f01cef30842cc236518c2c656d
box-monkey <jr.gasparin15@gmail.com>
2022-02-06 23:15:20 -0500

    began models and created some functionality

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,10 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    tag: {
+      type: DataTypes.INTEGER,
+
+    }
   },
   {
     sequelize,
@@ -18,3 +22,43 @@ Tag.init(
 );
 
 module.exports = Tag;
+
+/* 
+
+
+Tag
+    Doesn't allow null values
+
+    Set as primary key
+
+    Uses auto increment
+
+    tag_name
+
+    String
+
+ProductTag
+
+    id
+
+    Integer
+
+    Doesn't allow null values
+
+    Set as primary key
+
+    Uses auto increment
+
+    product_id
+
+    Integer
+
+    References the product model's id
+
+    tag_id
+
+    Integer
+
+    References the tag model's id
+
+*/ 
